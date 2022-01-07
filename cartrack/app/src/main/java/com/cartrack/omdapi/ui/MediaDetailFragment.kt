9f473e514cd2439e2020f81868e599bec550a58e
Ngoni Munyaradzi <ngoni.munyaradzi@rapiddeploy.com>
2022-01-07 11:34:50 +0200

    Code refactor

--- cartrack/app/src/main/java/com/cartrack/omdapi/ui/MediaDetailFragment.kt
+++ cartrack/app/src/main/java/com/cartrack/omdapi/ui/MediaDetailFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -53,15 +52,25 @@ class MediaDetailFragment : Fragment() {
         viewModel.media?.observe(viewLifecycleOwner, Observer {
             when (it.status) {
                 Resource.Status.SUCCESS -> {
-                    Toast.makeText(context, "${it.data}", Toast.LENGTH_SHORT).show()
                     hideProgressBar(binding.progressBar)
+                    binding.apply {
+                        mediaDetailContainer.visibility = View.VISIBLE
+                        resultsState.visibility = View.GONE
+                    }
+                    TODO("finish binding to detail screen")
                 }
                 Resource.Status.ERROR -> {
-                    Toast.makeText(context, "${it.message}", Toast.LENGTH_SHORT).show()
                     hideProgressBar(binding.progressBar)
+                    binding.resultsState.apply {
+                        text = it.message
+                        visibility = View.VISIBLE
+                    }
                 }
                 Resource.Status.LOADING -> {
-                    Toast.makeText(context, "Loading", Toast.LENGTH_SHORT).show()
+                    binding.apply {
+                        mediaDetailContainer.visibility = View.GONE
+                        resultsState.visibility = View.GONE
+                    }
                     showProgressBar(binding.progressBar)
                 }
             }
--- cartrack/app/src/main/java/com/cartrack/omdapi/ui/MediaListFragment.kt
+++ cartrack/app/src/main/java/com/cartrack/omdapi/ui/MediaListFragment.kt
@@ -4,18 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.cartrack.omdapi.utils.ViewUtils.hideProgressBar
-import com.cartrack.omdapi.utils.ViewUtils.showProgressBar
 import com.cartrack.omdapi.Constants.API_KEY_VALUE
 import com.cartrack.omdapi.Constants.KEY_API_KEY
 import com.cartrack.omdapi.Constants.KEY_SEARCH_TITLE
 import com.cartrack.omdapi.Constants.KEY_TYPE
+import com.cartrack.omdapi.R
 import com.cartrack.omdapi.databinding.FragmentMediaListBinding
 import com.cartrack.omdapi.utils.Resource
+import com.cartrack.omdapi.utils.ViewUtils.hideProgressBar
+import com.cartrack.omdapi.utils.ViewUtils.showProgressBar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -59,16 +59,26 @@ class MediaListFragment : Fragment() {
         viewModel.mediaList?.observe(viewLifecycleOwner, Observer {
             when (it.status) {
                 Resource.Status.SUCCESS -> {
-                    Toast.makeText(context, "${it.data}", Toast.LENGTH_SHORT).show()
                     hideProgressBar(binding.progressBar)
-                    adapter.setData(it.data)
+                    if (it.data?.isEmpty() == true) {
+                        binding.resultsState.apply {
+                            text = getString(R.string.no_results)
+                            visibility = View.VISIBLE
+                        }
+                    } else {
+                        adapter.setData(it.data)
+                        TODO("finish binding")
+                    }
                 }
                 Resource.Status.ERROR -> {
-                    Toast.makeText(context, "${it.message}", Toast.LENGTH_SHORT).show()
                     hideProgressBar(binding.progressBar)
+                    binding.resultsState.apply {
+                        text = it.message
+                        visibility = View.VISIBLE
+                    }
                 }
                 Resource.Status.LOADING -> {
-                    Toast.makeText(context, "Loading", Toast.LENGTH_SHORT).show()
+                    binding.resultsState.visibility = View.GONE
                     showProgressBar(binding.progressBar)
                 }
             }
