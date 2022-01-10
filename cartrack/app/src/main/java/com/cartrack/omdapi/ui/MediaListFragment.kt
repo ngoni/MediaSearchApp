0664bfc9792420550ac57eca466c79ae5fda1c78
Ngoni Munyaradzi <ngoni.munyaradzi@rapiddeploy.com>
2022-01-10 18:22:42 +0200

    Add logic for prompting user to search

--- cartrack/app/src/main/java/com/cartrack/omdapi/ui/MediaListFragment.kt
+++ cartrack/app/src/main/java/com/cartrack/omdapi/ui/MediaListFragment.kt
@@ -1,9 +1,12 @@
 package com.cartrack.omdapi.ui
 
+import android.content.Context
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -11,6 +14,8 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.cartrack.omdapi.Constants.API_KEY_VALUE
+import com.cartrack.omdapi.Constants.DEVICE_SETTINGS
+import com.cartrack.omdapi.Constants.FIRST_TIME_LOGIN
 import com.cartrack.omdapi.Constants.KEY_API_KEY
 import com.cartrack.omdapi.Constants.KEY_SEARCH_TITLE
 import com.cartrack.omdapi.Constants.KEY_TYPE
@@ -20,6 +25,7 @@ import com.cartrack.omdapi.databinding.FragmentMediaListBinding
 import com.cartrack.omdapi.utils.Resource
 import com.cartrack.omdapi.utils.ViewUtils.hideProgressBar
 import com.cartrack.omdapi.utils.ViewUtils.showProgressBar
+import com.google.android.material.appbar.MaterialToolbar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -27,6 +33,7 @@ class MediaListFragment : Fragment() {
 
     private lateinit var binding: FragmentMediaListBinding
     private lateinit var navController: NavController
+    private lateinit var toolbar: MaterialToolbar
     private lateinit var mediaListAdapter: MediaListAdapter
 
     private val viewModel: MediaViewModel by lazy {
@@ -41,7 +48,7 @@ class MediaListFragment : Fragment() {
 
         setupAdapter()
         initViews()
-        searchMedia()
+        shouldPromptUserToSearch()
         observeViewModel()
 
         return binding.root
@@ -50,12 +57,26 @@ class MediaListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navController = Navigation.findNavController(view)
+        initToolBar(view.rootView)
     }
 
     private fun initViews() {
         binding.recyclerview.apply {
             adapter = mediaListAdapter
             layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+
+    private fun initToolBar(view: View) {
+        toolbar = view.findViewById(R.id.toolbar)
+        toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.search -> {
+                    displaySearchDialog()
+                    true
+                }
+                else -> false
+            }
         }
     }
 
@@ -106,5 +127,42 @@ class MediaListFragment : Fragment() {
                 }
             }
         })
+    }
+
+    private fun shouldPromptUserToSearch() {
+        // We check to see if it's the first time a user is opening the app so we can show the Search Prompt
+        val prefs = requireActivity().getSharedPreferences(DEVICE_SETTINGS, Context.MODE_PRIVATE)
+        val firstTimeLogin = prefs.getBoolean(FIRST_TIME_LOGIN, true)
+        if (firstTimeLogin) {
+            displaySearchDialog()
+        }
+    }
+
+    private fun displaySearchDialog() {
+        // We update the value in SharedPrefs to indicate that the user has launched the app for the first time
+        requireActivity().getSharedPreferences(DEVICE_SETTINGS, Context.MODE_PRIVATE).apply {
+            edit().putBoolean(FIRST_TIME_LOGIN, false).apply()
+        }
+
+        val negativeBtnListener = fun(dialog: DialogInterface, value: Int) {
+            dialog.dismiss()
+        }
+
+        val positiveBtnListener = fun(dialog: DialogInterface, value: Int) {
+            dialog.dismiss()
+            searchMedia()
+        }
+
+        val customView: View =
+            LayoutInflater.from(requireContext()).inflate(R.layout.alertdialog_search_view, null)
+
+        val builder: AlertDialog.Builder = AlertDialog.Builder(requireContext())
+            .setTitle(getString(R.string.serch_content))
+            .setView(customView)
+            .setNegativeButton(getString(R.string.cancel), negativeBtnListener)
+            .setPositiveButton(getString(R.string.search), positiveBtnListener)
+            .setCancelable(true)
+        builder.create().show()
+
     }
 }
